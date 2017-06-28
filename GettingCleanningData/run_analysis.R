8300f9ffe718ebc81c4497cefc86eb12d9514235
motleis <m.tleis@liacs.leidenuniv.nl>
2017-06-28 22:08:58 +0200

    Updated run_anlaysis.R using read.table

--- GettingCleanningData/run_analysis.R
+++ GettingCleanningData/run_analysis.R
@@ -3,59 +3,32 @@ library(tidyr)
 ## I. merge training and test data
 
 # 1. load training data (subject, X and Y files)
-subject_train <- read.delim("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
-Y_train <- read.delim("UCI HAR Dataset/train/y_train.txt", header=FALSE)
-X_train <- read.delim("UCI HAR Dataset/train/x_train.txt", header=FALSE)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+Y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+X_train <- read.table("UCI HAR Dataset/train/x_train.txt")
 
 # 2. load the '561' feature names (to be used as column headers) 
-features <- read.delim("UCI HAR Dataset/features.txt", header=FALSE)
-features <- separate(features, V1, into=c("id", "value"), sep = " ")
+features <- read.table("UCI HAR Dataset/features.txt")
+colnames(features) <- c("id", "value")
 
-## 3. Remove all white space at the beginning and the end of the X_train data
-## these must be removed so that the split function won't recognize them as separate elements
-X_train_No_Surrounding_whitespace <- as.data.frame(sapply(X_train, function(x) {sub("^\\s+", "", x)}))
+## 3. load testing data (subject, X and Y files
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+Y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+X_test <- read.table("UCI HAR Dataset/test/x_test.txt")
 
-## 4. Split all the features in X_train into a vector. 
-## X_train contains values for 561 features
-## use 'separate' from the tidyr package
-X_train_tidy <- separate(X_train_No_Surrounding_whitespace, V1, into=features$value, sep="\\s+")
+## 4. Aggreate training & testing data (i.e. all the columns into one dataset)
+training <- cbind(subject_train, X_train, Y_train)
+testing <- cbind(subject_test, X_test, Y_test)
 
-# 5. add header names for training columns
-names(subject_train)[1] <- "Volunteer ID"
-names(Y_train)[1] <- "Activity"
-
-## 6. load testing data (subject, X and Y files
-subject_test <- read.delim("UCI HAR Dataset/test/subject_test.txt", header=FALSE)
-Y_test <- read.delim("UCI HAR Dataset/test/y_test.txt", header=FALSE)
-X_test <- read.delim("UCI HAR Dataset/test/x_test.txt", header=FALSE)
-
-## 7. Remove all white space at the beginning and the end of the X_train data
-## these must be removed so that the split function won't recognize them as separate elements
-X_test_No_Surrounding_whitespace <- as.data.frame(sapply(X_test, function(x) {sub("^\\s+", "", x)}))
-
-## 8. Split all the features in X_train into a vector. 
-## X_train contains values for 561 features
-## separate from tidyr package
-X_test_tidy <- separate(X_test_No_Surrounding_whitespace, V1, into=features$value, sep="\\s+")
-
-# 9. add header names
-names(subject_test)[1] <- "Volunteer ID"
-names(Y_test)[1] <- "Activity"
-
-## 10. Aggreate training data (i.e. all the columns into one dataset)
-training <- cbind(subject_train, X_train_tidy, Y_train)
-
-## 11. Aggreate testing data (i.e. all the columns into one dataset)
-testing <- cbind(subject_test, X_test_tidy, Y_test)
-
-## 12. Concatenate training and testing data
+## 5. Concatenate training and testing data
 full_dataset <- rbind(training, testing)
+colnames(full_dataset) <- c("Volunteer ID", as.character(features$value), "Activity")
 
 ## II. measurement on the Mean and StdDev of each measurement
 #. Extract the standard deviations measurement
 std_data <- full_dataset[ grep("std", colnames(full_dataset)) ]
 #. Extract the mean measurement
-mean_data <- full_dataset[ grep("std", colnames(full_dataset)) ]
+mean_data <- full_dataset[ grep("mean", colnames(full_dataset)) ]
 #. Extract the voluntter id column
 volunteer_id <- full_dataset[grep("Volunteer ID", colnames(full_dataset))]
 #. Extract the Activity column
@@ -65,13 +38,9 @@ activity <- full_dataset[grep("Activity", colnames(full_dataset))]
 mean_std <- cbind(volunteer_id, mean_data, std_data, activity)
 
 ## III. Use descriptive activity names to name the activities in the data set
-activity_labels<-read.delim("UCI HAR Dataset/activity_labels.txt", header=FALSE)
-activity_labels <- separate(activity_labels, V1, into=c("id", "value"), sep = " ")
-mean_if(mean_std$Activity==activity_labels$id) { mean_std$Activity <- activity_labels$value}
+activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
+colnames(activity_labels) <- c("id", "value")
 mean_std$Activity <- factor(mean_std$Activity, levels=c(1:6), labels = activity_labels$value)
-
-## IV. label the data with descriptive variable names
-#. All variables are already labeled with descriptive names in the previous steps
 
 ## V. A second independent tidy dataset with the average of each variable and each subject
 #. initialize the second tidy dataset that would hold the average mean values of the features in mean_std dataset
@@ -82,6 +51,7 @@ id <- vector('numeric')
 activities <- vector('character')
 # 3. Split the data per Volunteer ID
 split_by_volunteer<-split(mean_std, as.factor(mean_std$`Volunteer ID`) )
+
 # 4. Create a matrix to hold the computation of the mean for 
 # each feature. The number of rows is equal to the number of volunteers
 # mutliplied by the number of activities. The number of columns is equal

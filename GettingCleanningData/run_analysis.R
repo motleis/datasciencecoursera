76ef12211b04fb1f96339919f02d85a654eac192
motleis <m.tleis@liacs.leidenuniv.nl>
2017-06-29 21:52:33 +0200

    Use melt and dcast

--- GettingCleanningData/run_analysis.R
+++ GettingCleanningData/run_analysis.R
@@ -42,57 +42,16 @@ activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
 colnames(activity_labels) <- c("id", "value")
 mean_std$Activity <- factor(mean_std$Activity, levels=c(1:6), labels = activity_labels$value)
 
-## V. A second independent tidy dataset with the average of each variable and each subject
+## IV. A second independent tidy dataset with the average of each variable and each subject
 #. initialize the second tidy dataset that would hold the average mean values of the features in mean_std dataset
 
-# 1. create a vector to hold the volunteer id
-id <- vector('numeric')
-# 2. Create a character vector to hold the activity name
-activities <- vector('character')
-# 3. Split the data per Volunteer ID
-split_by_volunteer<-split(mean_std, as.factor(mean_std$`Volunteer ID`) )
+mean_std$`Volunteer ID` <- as.factor(mean_std$`Volunteer ID`)
 
-# 4. Create a matrix to hold the computation of the mean for 
-# each feature. The number of rows is equal to the number of volunteers
-# mutliplied by the number of activities. The number of columns is equal
-# to the number of features in the 'mean_std' dataset.
-features_matrix <- matrix(, nrow = length(split_by_volunteer)* length(activity_labels$value), ncol = (length(mean_std)-2) )
-# 5. Initialise a variable for the new rows to be added to the new dataset
-row.count <- 1
-# 6. loop through each volunteer
-for(i in 1 : length(split_by_volunteer) ) {
-  #. Create a data frame per each volunteer
-  df <- as.data.frame(split_by_volunteer[i])
-  #. Remove the autmatically added prefix to the beginnning of variable names
-  prefix <- paste("X", i , "." , sep="")
-  colnames(df)<-sub(prefix, "", colnames(df) )
-  #. Split each data frame per activity
-  split_per_activity<-split(df, as.factor(df$Activity) )
-  #. loop through each activity
-  for(j in levels(as.factor(df$Activity))) {
-    #. obtain the activity as a label
-    activity <- paste("", j , sep="")
-    #. Obtain a datafraem per each activity for each volunteer
-    act.df<-as.data.frame(split_per_activity[activity])
-    #. Create a numeric vector to hold averages of all features
-    averages <- vector('numeric')
-    #. Loop through all columns in df 
-    nfeatures<-ncol(act.df)-2
-    for (x in 1:nfeatures) {
-      averages[x] <- mean(as.numeric(as.character(act.df[,(x+1)] ) ))
-    }
-    #. Create an instance with id of volunteer and the activity
-    # in addition to averages of all mean and std features
-    id[row.count] <- i
-    activities[row.count] <- j
-    features_matrix[row.count,] <- averages
-    row.count <- row.count+1
-  }
-  #. Combine the vectors into one data frame
-  feature_averages<-as.data.frame(cbind(id, features_matrix, activities))
-  #. Add the column names and append 'Avg' for the new feature column names
-  colnames(feature_averages) <- colnames(mean_std)
-  names(feature_averages) <- sub("^t", "Avg_t", names(feature_averages))
-  names(feature_averages) <- sub("^f", "Avg_f", names(feature_averages))
+allData.melted <- melt(mean_std, id = c("Volunteer ID", "Activity"))
+allData.mean <- dcast(allData.melted, `Volunteer ID` + Activity ~ variable, mean)
+
+#. append 'Avg' to feature names
+  names(allData.mean) <- sub("^t", "Avg_t", names(allData.mean))
+  names(allData.mean) <- sub("^f", "Avg_f", names(allData.mean))
 }
-write.csv(feature_averages, "final_tidy_data.csv")
+write.csv(allData.mean, "final_tidy_data.csv")
